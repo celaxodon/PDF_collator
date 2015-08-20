f4267d06647fdd268a589cc1100c75b2a718cfc5
Graham Leva <g.ravel66@gmail.com>
2015-08-20 18:52:48 -0500

    Write title and first section of python version

---
+++ PDF_collator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#---------------------------------------#
+#           PDF Collator v.01           #
+#        Written by Graham Leva         #
+#     Copyright (c) 2015 AnalySys, Inc. #
+# Something about the proper license    #
+#---------------------------------------#
+
+#-------------------#
+# Dependency Checks #
+#-------------------#
+
