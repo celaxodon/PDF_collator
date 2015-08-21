ba0166e03a036f2ab03e8eaf50fbc5d2a8f4fd20
Graham Leva <g.ravel66@gmail.com>
2015-08-20 22:11:10 -0500

    Add function headers to mimic bash script

--- PDF_collator.py
+++ PDF_collator.py
@@ -7,7 +7,54 @@
 # Something about the proper license    #
 #---------------------------------------#
 
+import sys, ghostscript, logging
+
 #-------------------#
 # Dependency Checks #
 #-------------------#
+
+# Check for ghostscript
+
+# Check for correct volumes mounted
+
+#/Volumes/scans
+
+#/Volumes/data
+
+#/volumes/admin
+
+# Variables
+
+ToPDF =
+ToFile =
+ToStrip = 
+CoC_dir = 
+Billings = 
+
+# check necessary directories exist
+# ToPDF directory
+
+# ToFile directory
+
+# ToStrip directory
+
+# CoCDirectory
+
+# Billings
+
+#------#
+# Help #
+#------#
+
+#---------------#
+# Name Stripper #
+#---------------#
+
+#-------------#
+# CoC Grabber #
+#-------------#
+
+#-----------------#
+# Collect Reports #
+#-----------------#
 
