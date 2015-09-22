194930dcb508e42b2d22f3e98c0b319579ca6de1
Graham Leva <g.ravel66@gmail.com>
2015-09-22 10:39:19 -0500

    Add basic framework for testing in test ssuite

--- test_collator.py
+++ test_collator.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
 import unittest
+from PDF_collator import system_checks
+
+class SystemCheckTest(unittest.TestCase):
+    """Class for testing system checks work in PDF_collator.py."""
+
+    def testDirs(self):
+
+if __name__ == '__main__':
+    unittest.main()
