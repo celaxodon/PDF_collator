e40723fba6fa359fbd11074b9e9c1aa11bc6015e
Graham Leva <g.ravel66@gmail.com>
2015-09-30 10:45:07 -0500

    Rewrite file and system checks, reformat docstrings

--- PDF_collator.py
+++ PDF_collator.py
@@ -7,9 +7,9 @@
 # Something about the proper license    #
 #---------------------------------------#
 
-import sys, os.path, logging, argparse, re
+import sys, os, os.path, logging, argparse, re
 # For color output
-import colorama
+#import colorama
 
 #***********#
 # Variables #
@@ -26,62 +26,94 @@ BILLINGS = '/Volumes/Admin/Billings/'
 
 
 def system_checks():
-    """ Function for checking required software is installed
-        and that remote file system directories are mounted """
+    """Function for checking required software is installed
+       and that remote file system directories are mounted."""
 
-    # Check for OS version for compatibility
-    # Try block?
+    # Check operating system
+    if os.uname().sysname != 'Darwin':
+        print("Warning! This program was only designed to run on Mac OS X")
+        ans = input("Run at your own risk. Continue? (y/n)\n")
+        while True:
+            low_ans = str(ans).lower()
+            if low_ans == 'y' or low_ans == 'yes':
+                # Continue with checks
+                break
+            elif low_ans == 'n' or low_ans == 'no':
+                return False
+            else:
+                print("Yes ('y') or no ('n'), please.")
+                ans = input("Continue program? (y/n)\n")
 
-    if os.path.exists('/Volumes/Data/') != True:
-        print("This program cannot run unless you have the 'Data' folder mounted.")
-        print("Please connect to the file server (New Server) and run this\
-               program again.")
-        return
-    elif os.path.exists('/Volumes/scans/') != True:
-        print("This program cannot run unless you have the 'scans' folder mounted.")
-        print("Please connect to the file server (New Server) and run this\
-               program again.")
-        return
-    else os.path.exists('/Volumes/Admin/') != True:
-        print("This program cannot run unless you have the 'Admin' folder mounted.")
-        print("Please connect to the file server (New Server) and run this\
-               program again.")
-        return
+    # Check correct volumes from file server mounted
+    elif os.path.exists('/Volumes/Data/') == False:
+        print("This program cannot run unless you have the 'Data' folder "
+              "mounted.")
+        print("Please connect to the file server (\'New Server\') and run this"
+              " program again.\n")
+        return False
+
+    elif os.path.exists('/Volumes/scans/') == False:
+        print("This program cannot run unless you have the 'scans' folder "
+              "mounted.")
+        print("Please connect to the file server (\'New Server\') and run this"
+              " program again.\n")
+        return False
+
+    elif os.path.exists('/Volumes/Admin/') == False:
+        print("This program cannot run unless you have the 'Admin' folder "
+              "mounted.")
+        print("Please connect to the file server (\'New Server\') and run this" 
+               "  program again.\n")
+        return False
+    else:
+        return True
 
     # Test that specific directories exist
     for folder in [FIN_REPORTS, REVD_REPORTS, COC_DIR, BILLINGS]:
-        if os.path.exists(folder) != True:
-            print("The folder {0} is not accessible. Please make sure you can\
-                   navigate to the folder before running this\
-                   program again.".format(folder))
-            return
+        if os.path.exists(folder) == False:
+            print("The folder {0} is not accessible. Please make sure you can"
+                  " navigate to the folder before running this"
+                  " program again.\n".format(folder))
+            return False
         else:
             continue
 
-    # Test for files existing in REVD_REPORTS - if none exist, program exits
-    # Reconsider where to put this check and overall flow for system_checks func.
-    if len(os.listdir(REVD_REPORTS)) == 0:
-        return "No files exist in the reviewed reports folder. Program exiting."
+
+def file_check(directory):
+    """Test for files existing in target directory."""
+
+    if len(os.listdir(directory)) == 0:
+        print("No files exist in the reviewed reports folder for collation.\n")
+        return False
+    else:
+        return True    # Files exist
+
+    # Consider checking for .afp_[\d]+ files in collation directory
 
 
-# Argument parser for command line invocation
 def parser_setup():
-    parser = argparse.ArgumentParser(description='Rename scanned reports, find Chain of Custodies (CoCs) and collate PDF reports.')
+    """Parser for command line invocation and arguments."""
+
+    parser = argparse.ArgumentParser(description='Rename scanned reports, find'
+            ' Chain of Custodies (CoCs) and collate PDF reports.')
     # -h, --help is setup by default
-    parser.add_argument('-c', '--clean', help="Clean up temporary directories and\
-                        reset files to starting positions.", action="store_true")
+    parser.add_argument('-c', '--clean', help='Clean up temporary directories '
+                        'and reset files to starting positions.',
+                        action="store_true")
     args = parser.parse_args()
     if args.clean:
         # do something
         print("Cleaning in progress...")
+        print('...')
         clean()
-        print()
         print("Cleaning complete.")
 
 
 def clean():
-    """ Clean the working directory and reset files back to their starting
-        positions. """
+    """Clean the working directory and reset files back to their starting
+       positions. """
+    # Note, if properly handling exceptions and errors, and also maybe using
+    # temporary file systems, this method could become obsolete.
     pass
 
 #***************#
@@ -89,42 +121,56 @@ def clean():
 #***************#
 
 def strip_chars(directory):
-    """ Strip leading characters from file names in a specified directory. 
-        Pattern is of the form 'job_####', where '#' can be any number of 
-        numbers, but usually less than five."""
+    """Strip leading characters from file names in a specified directory. 
+       Pattern is of the form 'job_####', where '#' can be any number of 
+       numbers, but usually less than five."""
 
-    regex = re.compile('job_[\d]*')
-
-    for item in os.path(directory):
-        if str(filename)[0:4] == 'job_':
-            #os.rename(<src>, <dest>)
-
+    # Check syntax for backslashes - compile to \\w and \\d
+    regex = re.compile('^job_[\d]*[\w]{1}')
+    file_list = os.listdir(directory)
+    for i in file_list:
+        if regex.match(i):
+            i = 
+            # remember to join paths, if you're not working from that directory
+            os.rename(old, new)
 
 
 #***************#
 # CoC Collector #
 #***************#
+
+def collect_cocs(directory, file_list):
+    """Collects Chain of Custody files given a target directory and list of
+       files to match against CoCs."""
+
+#******************#
+# Report Collector #
+#******************#
 # Standard coc filename len = 13 --> 123456coc.pdf
 # Multi-PDF coc filename len = 17 --> 123450-456coc.pdf
 # Single rerun coc filename len = 14 --> 123456acoc.pdf
 # Multi-PDF rerun coc filename len = 19 --> 123450a-456acoc.pdf
 # QC/WP/SP samples have NO RANGES, but take the form QC###-###coc.pdf. len = 16
 
-#******************#
-# Report Collector #
-#******************#
+
+def main():
+
+    parser_setup()
+
+    # Make system checks
+    if system_checks() == False:
+        return "System checks failed. Program exiting."
+    elif file_check() == False:
+        return "Program exiting."
+        exit()
+
+    print("Stripping file names...\n")
+
+    strip_chars(REVD_REPORTS)
+
 
 # "Main" when running by itself. Should be the default operating mode, since
 # users are simply clicking on the script to execute
 if __name__ == '__main__':
 
-    # Check for required file system locations and mount points
-    system_checks()
-    # Set up CLI argument parser
-    parser_setup()
-    # Test whether files exist before running name stripper.
-    try:
-        # If files exist, run program
-
-    except:
-        # Some exception for files not existing
+    main()
